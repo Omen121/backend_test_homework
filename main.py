98e67bfcc7052e7b80214845525a27b0a09bc1e4
Вячеслав Шведов <i@omen-121.ru>
2022-05-17 20:41:51 +0300

    Complete PEP8

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+
+MESSAGE = ('Добро пожаловать в самую лучшую программу для вычисления'
+           'квадратного корня из заданного числа')
+
+
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return math.sqrt(number)
+
+
+def calc(your_number):
+    """Проверяет, положительно ли число."""
+    if your_number <= 0:
+        return
+    print(f'Мы вычислили корень квадратный из введенного вами '
+          f'числа. Это будет: {calculate_square_root(your_number)}')
+
+
+print(MESSAGE)
+calc(25.5)
